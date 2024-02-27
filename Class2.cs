d1b063410959700aeb588fa24cda7c899a5436ad
Riiivv <156653527+Riiivv@users.noreply.github.com>
2024-02-27 14:00:49 +0100

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class DKamp
+    {
+        public string navn1;
+        public string navn2;
+        public int point1;
+        public int point2;
+        public int sum;
+    }
+}
